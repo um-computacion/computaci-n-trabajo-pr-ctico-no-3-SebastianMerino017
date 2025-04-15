77db126683f4bb6f110f8eb74eab537ecaac79c9
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-04-15 15:22:39 -0300

    Implementacion de prueba para letras

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -21,4 +21,10 @@ class TestCalculoNumeros(unittest.TestCase):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
 
+     @patch('builtins.input', return_value='AAA')
+    def test_ingreso_letras(self, patch_input):
+        from src.exceptions import ingrese_numero
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+
  
