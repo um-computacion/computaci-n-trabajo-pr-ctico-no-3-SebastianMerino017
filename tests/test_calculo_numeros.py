742184be95bd7908586d3a26a1e08c5fd35dcc8d
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2025-04-15 15:29:14 -0300

    Implementacion de prueba para caracter vacio

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -21,10 +21,17 @@ class TestCalculoNumeros(unittest.TestCase):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
 
-     @patch('builtins.input', return_value='AAA')
+    @patch('builtins.input', return_value='AAA')
     def test_ingreso_letras(self, patch_input):
         from src.exceptions import ingrese_numero
         with self.assertRaises(ValueError):
             ingrese_numero()
+    
+    @patch('builtins.input', return_value='')
+    def test_ingreso_vacio(self, patch_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+
+
 
  
